b5994cdea45f5860da215b57d6aa4841aefb744e
etrafa <84640450+etrafa@users.noreply.github.com>
2021-07-07 21:09:19 +0300

    add cv files

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const btnAgain = document.querySelector(".btn again");
+const btnCheck = document.querySelector(".btn check");
+
+btnAgain.addEventListener("click", function () {
+  console.log("hey");
+});
+
+btnCheck.addEventListener("click", function () {
+  console.log("hi");
+});
